56301e98773496f1d1703ba63c03eba536120e6f
Stu Blair <stu314159@gmail.com>
2020-10-13 16:15:22 -0400

    add FB expansion using scipy.special.jv instead of j1

--- fourier_bessel2.py
+++ fourier_bessel2.py
@@ -4,12 +4,14 @@ Written by: CAPT Stu Blair
 Date: 12 Oct
 Purpose: simple Fourier-Bessel series expansion to a function
 
+This alternative implementation uses the more general jv function
+
 """
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.integrate as integrate
 from scipy.special import jn_zeros 
-from scipy.special import j1 
+from scipy.special import jv 
 
 
 
@@ -19,7 +21,7 @@ def f_exp(x):
 def FB_exp(x,cn,ev,N):
     ret_val = 0.;
     for i in range(N):
-        ret_val += cn[i]*j1(x*ev[i]);
+        ret_val += cn[i]*jv(1,x*ev[i]);
     return ret_val;
     
 # describe discrete domain
@@ -39,8 +41,8 @@ ev = k/b;
 cn = np.zeros_like(ev);
 for n in range(N):
     # compute all of the expansion coefficients
-    num, err = integrate.quad(lambda x: f_exp(x)*j1(ev[n]*x)*x,a,b);
-    denom, err = integrate.quad(lambda x: j1(ev[n]*x)*j1(ev[n]*x)*x,a,b);
+    num, err = integrate.quad(lambda x: f_exp(x)*jv(1,ev[n]*x)*x,a,b);
+    denom, err = integrate.quad(lambda x: jv(1,ev[n]*x)*jv(1,ev[n]*x)*x,a,b);
     cn[n] = num/denom;
    
 # make a convenient function capturing the FB expansion
